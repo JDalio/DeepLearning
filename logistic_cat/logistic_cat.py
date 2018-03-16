84771d8f186bf2e3b046523810fd2724175a2119
jhz <2267264176@qq.com>
2018-03-16 22:51:47 +0800

    jhz's path to deep learning.
    Signed-off-by: jhz <2267264176@qq.com>

---
+++ logistic_cat/logistic_cat.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pylab as plt
+from lr_utils import load_dataset
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def initialize_with_zeros(dim):
+    w = np.zeros([dim, 1])
+    b = 0
+    return w, b
+
+
+def propagate(w, b, X, Y):
+    m = X.shape[1]
+
+    # FORWARD PROPAGATION
+    A = sigmoid(np.dot(w.T, X) + b)
+    cost = (-1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))
+
+    # BACKWARD PROPAGATION
+    dw = (1 / m) * np.dot(X, (A - Y).T)
+    db = (1 / m) * np.sum(A - Y)
+
+    cost = np.squeeze(cost)
+
+    return dw, db, cost
+
+
+def optimize(w, b, X, Y, times, rate, print_cost=False):
+    costs = []
+    for i in range(times):
+        dw, db, cost = propagate(w, b, X, Y)
+        w -= rate * dw
+        b -= rate * db
+
+        if i % 100 == 0:
+            costs.append([i / 100, cost])
+
+        if print_cost and i % 100 == 0:
+            print('Cost :', i, 'times:', cost)
+
+    params = {'w': w, 'b': b}
+
+    grads = {'dw': dw, 'db': db}
+
+    return params, grads, costs
+
+
+def accuracy(w, b, X, data):
+    m = X.shape[1]
+    Y_prediction = np.zeros([1, m])
+    A = sigmoid(np.dot(w.T, X) + b)
+
+    for i in range(m):
+        Y_prediction[0][i] = 1 if A[0, i] > 0.5 else 0
+
+    return 100 - np.mean(np.abs(Y_prediction - data)) * 100
+
+
+def showCost(costs, rate):
+    plt.xlabel('iterations (per hundreds)')
+    plt.ylabel('cost')
+    plt.title('Learning rate = ' + str(rate))
+    for i in range(len(costs)):
+        plt.plot(costs[i][0], costs[i][1], 'ob')
+    plt.show()
+
+
+def saveResults(w, b):
+    res = np.vstack([b, w])
+    np.savetxt('iscat.txt', res, delimiter=',')
+
+
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+# 训练集中样本个数
+m_train = train_set_y.shape[1]
+# 测试集中样本个数
+m_test = test_set_y.shape[1]
+# 每个图片为num_px*num_px的
+num_px = train_set_x_orig.shape[1]
+
+train_set_x_flatten = train_set_x_orig.reshape(m_train, -1).T
+test_set_x_flatten = test_set_x_orig.reshape(m_test, -1).T
+
+train_set_x = train_set_x_flatten / 255
+test_set_x = test_set_x_flatten / 255
+
+dim = num_px ** 2 * 3
+w, b = initialize_with_zeros(dim)
+rate = 0.005
+times = 950
+X = train_set_x
+Y = train_set_y
+
+params, grads, costs = optimize(w, b, X, Y, times, rate, True)
+
+w = params['w']
+b = params['b']
+
+print("Times:", times)
+print("Train Accuracy:", accuracy(w, b, X, train_set_y))
+print("Test Accuracy:", accuracy(w, b, test_set_x, test_set_y))
+
+print(w, b)
+saveResults(w, b)
+
+showCost(costs, rate)
---
+++ logistic_regression/logistic.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pylab as plt
+
+
+def sigmoid(x):
+    return 1.0 / (1 + np.exp(-x))
+
+
+def loadData():
+    data = np.loadtxt('dataset/logistic_regression.txt', delimiter=',')
+    train_x = data[..., 0:2]
+    train_y = data[..., 2]
+    train_y = train_y.reshape([train_y.shape[0], 1])
+    return [train_x.T, train_y.T]
+
+
+def trainLogRegres(train_x, train_y, opts):
+    alpha = opts['alpha']
+    maxIter = opts['maxIter']
+    weights = np.zeros([train_x.shape[0], 1])
+    bias = 0.0
+    print('Init :\nWeights :\n', weights, '\nBias :', bias)
+    for i in range(maxIter):
+        a = sigmoid(np.dot(weights.T, train_x))
+        error = train_y - a
+
+        bias += alpha * np.mean(error)
+        weights += alpha * np.dot(train_x, error.T)
+    return weights, bias
+
+
+def testLogRegres(res, test_x, test_y):
+    featureNum, sampleNum = np.shape(test_x)
+    matchCount = 0
+    for i in range(sampleNum):
+        predict = sigmoid(np.dot(res[0].T, test_x[..., i]) + res[1]) > 0.5
+        if (predict == bool(test_y[..., i])):
+            matchCount += 1
+    return float(matchCount) / sampleNum
+
+
+def showLogRegres(res, train_x, train_y):
+    featureNum, sampleNum = np.shape(train_x)
+    print(sampleNum)
+    for i in range(sampleNum):
+        if train_y[0, i] == 0:
+            plt.plot(train_x[..., i][0], train_x[..., i][1], 'or')
+        else:
+            plt.plot(train_x[..., i][0], train_x[..., i][1], 'ob')
+
+    min_x = np.min(train_x[0])
+    max_x = np.max(train_x[0])
+    a1, a2 = res[0]
+    b = res[1]
+    min_y = -(a1 * min_x + b) / a2
+    max_y = -(a1 * max_x + b) / a2
+
+    plt.plot([min_x, max_x], [min_y, max_y], '-g')
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
+
+## step 1: load data
+train_x, train_y = loadData()
+# print(np.dot(np.array([1,3]).reshape([1,2]),train_x[...,0]).shape)
+
+## step 2： training
+opts = {'alpha': 0.001, 'maxIter': 4000, 'optimizeType': 'GradDescent'}
+res = trainLogRegres(train_x, train_y, opts)
+# print(res[0].shape)
+
+## step 3: testing
+accuracy = testLogRegres(res, train_x, train_y)
+# print(accuracy)
+
+## step 4: show the result
+print('Result:\n', 'Weights:\n', res[0], '\nBias:', res[1])
+print('Accuracy:', accuracy)
+showLogRegres(res, train_x, train_y)
