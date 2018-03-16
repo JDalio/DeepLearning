3b4dca6c1740afed0380454e7aa4eb1ef4e2171e
jhz <2267264176@qq.com>
2018-03-16 22:29:14 +0800

    jhz's path to deep learning.

--- dl.py
+++ dl.py
@@ -1,51 +1,37 @@
-import cv2
-import os
 import numpy as np
-import tensorflow as tf
 
-def CompressImg(dir):
-    matrix = np.zeros([200 * 200, 1],dtype='f8')
-    files = os.listdir(dir)
-    for f in files:
-        path=dir + "/"+f
-        if not os.path.isdir(path):
-            img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-            img=np.array(img,dtype='f8')
-            img=img.reshape(img.size,1)
-            matrix=np.hstack([matrix,img])
-        else:
-            matrix=np.hstack([matrix,CompressImg(path)])
-    return matrix[...,1:]
 
-def relu(A):
-    return np.maximum(A,np.zeros(A.shape))
-#set=CompressImg('dataset')
-img=cv2.imread('dataset.png',cv2.IMREAD_GRAYSCALE)
-set=np.array(img,dtype='f8')
+def optimizer(data, b, m, rate, times):
+    for i in range(times):
+        [b, m] = compute_gradient(b, m, data, rate)
+        print(i, 'times:', [b, m])
+    return [b, m]
 
-T1=np.hstack([np.ones([1,30]),np.zeros([1,10])])
-T2=np.zeros([1,40])
-T2[0][6:10]=1
-T2[0][39]=1
-T2[0][35]=1
-T=np.vstack([T1,T2])
-len=set.shape[0]
-features=2
-W=np.random.randn(len,features)
-B=np.random.randn(features,40)
-for i in range(10):
-    #Forward Propegation
-    Y=np.dot(W.T,set)+B
-    a=relu(Y)
-    print(a)
-'''
-    loss=-(T*np.log(a)+(1-T)*np.log(1-a))
-    cost=np.mean(loss,1).reshape(2,1)
-    #backward Propegation
-    rate=0.5
-    db=(a-T)
-    dw=np.dot(set,(a-T).T)
-    W-=rate*dw
-    B-=rate*db
-    print(loss)
-'''
+
+def compute_gradient(b_cur, m_cur, data, rate):
+    b_gra = 0
+    m_gra = 0
+
+    N = float(len(data))
+
+    for i in range(len(data)):
+        x = data[i, 0]
+        y = data[i, 1]
+
+        b_gra += -(2 / N) * (y - (m_cur * x + b_cur))
+        m_gra += -(2 / N) * x * (y - (m_cur * x + b_cur))
+
+    new_b = b_cur - rate * b_gra
+    new_m = m_cur - rate * m_gra
+
+    return [new_b, new_m]
+
+
+data = np.loadtxt('./dataset/linear_regression.txt', delimiter=',')
+
+rate = 0.01
+b = 0.0
+m = 0.0
+times = 1000
+
+[b, m] = optimizer(data, b, m, rate, times)
